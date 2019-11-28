f053e0fd5e695936fe23aad1e5557ec428e393ce
陈志鹏 <huyesinian@outlook.com>
2019-11-28 21:05:37 +0800

    添加项目文件。

---
+++ TheWorldTree/Data/TheWorldTreeDBContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TheWorldTree.Data
+{
+    public class TheWorldTreeDBContext : DbContext
+    {
+        public TheWorldTreeDBContext(DbContextOptions<TheWorldTreeDBContext> options) : base(options)
+        {
+            if (Database.GetPendingMigrations().Any())
+            {
+                //执行迁移
+                Database.Migrate();
+            }
+        }
+
+
+        //public DbSet<Movie> Movie { get; set; }
+
+    }
+}
